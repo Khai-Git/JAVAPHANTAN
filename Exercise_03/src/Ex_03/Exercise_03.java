2d6de0f2913d31f0e3d9ef805e4528024f7f6db2
Khai-Git <kelvinnon28092001@gmail.com>
2024-01-10 07:41:35 +0700

    Ex_03

--- Exercise_03/src/Ex_03/Exercise_03.java
+++ Exercise_03/src/Ex_03/Exercise_03.java
@@ -3,33 +3,84 @@ package Ex_03;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
-class Bank {
+class BankAccount {
 	private int balance = 0;
 	private Lock lock = new ReentrantLock();
-	
+
 	public void deposits(int n) {
 		lock.lock();
-		
-		balance += n;
-		
-		lock.unlock();
+
+		try {
+			balance += n;
+			System.out.println("Deposit: " + n + ", Balance: " + balance);
+		} finally {
+			lock.unlock();
+		}
 	}
-	
+
 	public void withdraws(int n) {
 		lock.lock();
-		
-		balance -= n;
-		
-		lock.unlock();
+
+		try {
+			balance -= n;
+			System.out.println("Withdraw: " + n + ", Balance: " + balance);
+		} finally {
+			lock.unlock();
+		}
 	}
 }
 
-class DepositThread extends Thread {
-	
+class DepositsThread extends Thread {
+	private BankAccount account;
+
+	public DepositsThread(BankAccount account) {
+		super();
+		this.account = account;
+	}
+
+	public synchronized void run() {
+		account.deposits(100);
+
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
+
+class WithDrawsThread extends Thread {
+	private BankAccount account;
+
+	public WithDrawsThread(BankAccount account) {
+		super();
+		this.account = account;
+	}
+
+	public synchronized void run() {
+		account.withdraws(100);
+
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
 
 public class Exercise_03 {
-	public static void main(String[] args) {
-		System.out.println();
+	public static void main(String[] args) throws InterruptedException {
+		BankAccount account = new BankAccount();
+
+		for (int i = 0; i < 5; i++) {
+			new DepositsThread(account).start();
+			Thread.sleep(1000);
+		}
+		for (int i = 0; i < 5; i++) {
+			new WithDrawsThread(account).start();
+			Thread.sleep(1000);
+		}
 	}
 }
