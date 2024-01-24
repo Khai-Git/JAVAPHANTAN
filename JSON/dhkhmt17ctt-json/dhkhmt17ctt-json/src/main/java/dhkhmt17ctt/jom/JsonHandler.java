289fae5d302db5862f54e83aea9ef200f05b520e
Khai-Git <kelvinnon28092001@gmail.com>
2024-01-24 08:59:13 +0700

    Fixing error address = null & phoneNumber = null in getPersons method

--- JSON/dhkhmt17ctt-json/dhkhmt17ctt-json/src/main/java/dhkhmt17ctt/jom/JsonHandler.java
+++ JSON/dhkhmt17ctt-json/dhkhmt17ctt-json/src/main/java/dhkhmt17ctt/jom/JsonHandler.java
@@ -20,215 +20,208 @@ import jakarta.json.JsonValue;
 import jakarta.json.JsonWriter;
 
 public class JsonHandler {
-	
-	
+
 	public static String toJson(Person p) {
 		StringWriter st;
-		
-		try(JsonWriter writer = Json.createWriter(st = new StringWriter())){
+
+		try (JsonWriter writer = Json.createWriter(st = new StringWriter())) {
 			JsonObjectBuilder oBuilder = Json.createObjectBuilder();
-			
+
 			Address add = p.getAddress();
-			
-			JsonObject joAdd = oBuilder
-			.add("streetAddress", add.getStreetAddress())
-			.add("city", add.getCity())
-			.build();
-			
-			
+
+			JsonObject joAdd = oBuilder.add("streetAddress", add.getStreetAddress()).add("city", add.getCity()).build();
+
 			JsonObject jo = oBuilder.add("firstName", p.getFirstName())
-			.add("age", p.getAge())
-			.add("address", joAdd)
-			
-			.build();
-			
+					.add("lastName", p.getLastName())
+					.add("age", p.getAge()).add("address", joAdd)
+					.build();
+
 			writer.writeObject(jo);
 //			writer.writeObject(oBuilder.build());
 		}
-		
+
 		return st.toString();
-	}	
-	
+	}
+
 	public static List<Person> getPersons(String fileName) {
-		
+
 		List<Person> list = null;
-		
-		try(JsonReader reader = Json.createReader(new FileReader(fileName))){
-			
+
+		try (JsonReader reader = Json.createReader(new FileReader(fileName))) {
+
 			JsonArray ja = reader.readArray();
-			if(ja != null)
+			if (ja != null)
 				list = new ArrayList<>();
-			
-			for(JsonValue jv : ja) {
-				
+
+			for (JsonValue jv : ja) {
+
 				Person p = new Person();
-				
-				if(jv instanceof JsonObject) {
-					JsonObject jo = (JsonObject)jv;
+
+				if (jv instanceof JsonObject) {
+					JsonObject jo = (JsonObject) jv;
 					p.setFirstName(jo.getString("firstName"));
+					p.setLastName(jo.getString("lastName"));
 					p.setAge(jo.getInt("age"));
 					list.add(p);
 				}
+				
 			}
-			
-		}catch (IOException e) {
+
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		return list;
 	}
+
 	private static JsonObjectBuilder createPersonJsonObject(Person p) {
-        JsonObjectBuilder oBuilder = Json.createObjectBuilder()
-                .add("firstName", p.getFirstName())
-                .add("lastName", p.getLastName())
-                .add("age", p.getAge());
+		JsonObjectBuilder oBuilder = Json.createObjectBuilder()
+				.add("firstName", p.getFirstName())
+				.add("lastName", p.getLastName())
+				.add("age", p.getAge());
 
-        Address add = p.getAddress();
+		Address add = p.getAddress();
 
-        if (add != null) {
-            JsonObject joAdd = Json.createObjectBuilder()
-                    .add("streetAddress", add.getStreetAddress())
-                    .add("city", add.getCity())
-                    .add("state", add.getState())
-                    .add("postalCode", add.getPostalCode())
-                    .build();
+		if (add != null) {
+			JsonObject joAdd = Json.createObjectBuilder().add("streetAddress", add.getStreetAddress())
+					.add("city", add.getCity())
+					.add("state", add.getState())
+					.add("postalCode", add.getPostalCode())
+					.build();
 
-            oBuilder.add("address", joAdd);
-        }
-        List<PhoneNumber> phoneNumbers = p.getPhoneNumbers();
-        if (phoneNumbers != null && !phoneNumbers.isEmpty()) {
-            JsonArrayBuilder phoneNumbersArrayBuilder = Json.createArrayBuilder();
-            phoneNumbers.forEach(phoneNumber -> {
-                JsonObject phoneNumberJson = Json.createObjectBuilder()
-                        .add("type", phoneNumber.getType())
-                        .add("number", phoneNumber.getNumber())
-                        .build();
-                phoneNumbersArrayBuilder.add(phoneNumberJson);
-            });
-            oBuilder.add("phoneNumbers", phoneNumbersArrayBuilder.build());
-        }
+			oBuilder.add("address", joAdd);
+		}
+		List<PhoneNumber> phoneNumbers = p.getPhoneNumbers();
+		if (phoneNumbers != null && !phoneNumbers.isEmpty()) {
+			JsonArrayBuilder phoneNumbersArrayBuilder = Json.createArrayBuilder();
+			phoneNumbers.forEach(phoneNumber -> {
+				JsonObject phoneNumberJson = Json.createObjectBuilder().add("type", phoneNumber.getType())
+						.add("number", phoneNumber.getNumber())
+						.build();
+				phoneNumbersArrayBuilder.add(phoneNumberJson);
+			});
+			oBuilder.add("phoneNumbers", phoneNumbersArrayBuilder.build());
+		}
 
-        return oBuilder;
-    }
-	
+		return oBuilder;
+	}
+
 	public static String toJson(List<Person> persons) {
-        StringWriter st;
+		StringWriter st;
 
-        try (JsonWriter writer = Json.createWriter(st = new StringWriter())) {
-            JsonArrayBuilder aBuilder = Json.createArrayBuilder();
+		try (JsonWriter writer = Json.createWriter(st = new StringWriter())) {
+			JsonArrayBuilder aBuilder = Json.createArrayBuilder();
 
-            persons.forEach(p -> {
-                JsonObjectBuilder oBuilder = createPersonJsonObject(p);
-                aBuilder.add(oBuilder.build());
-            });
+			persons.forEach(p -> {
+				JsonObjectBuilder oBuilder = createPersonJsonObject(p);
+				aBuilder.add(oBuilder.build());
+			});
 
-            JsonArray ja = aBuilder.build();
-            writer.writeArray(ja);
-        }
+			JsonArray ja = aBuilder.build();
+			writer.writeArray(ja);
+		}
 
-        return st.toString();
-    }
-	
+		return st.toString();
+	}
+
 	public static void toJsonFile(List<Person> persons, String fileName) {
-		
-		try(JsonWriter writer = Json.createWriter(new FileWriter(fileName))){
-			
+
+		try (JsonWriter writer = Json.createWriter(new FileWriter(fileName))) {
+
 			JsonObjectBuilder oBuilder = Json.createObjectBuilder();
 			JsonArrayBuilder aBuilder = Json.createArrayBuilder();
-			
+
 			persons.forEach(p -> {
-				
+
 				Address add = p.getAddress();
 				JsonObject joAdd = null;
-				if(add != null) {
+				if (add != null) {
 					joAdd = oBuilder.add("city", add.getCity())
-					.add("state", add.getState())
-					.build();
+							.add("state", add.getState())
+							.build();
 				}
 				oBuilder.add("firstName", p.getFirstName())
+				.add("lastName", p.getLastName())
 				.add("age", p.getAge());
-				
-				if(joAdd != null)
-					oBuilder.add("address",joAdd);
-				
+
+				if (joAdd != null)
+					oBuilder.add("address", joAdd);
+
 				JsonObject jo = oBuilder.build();
-				
+
 				aBuilder.add(jo);
 			});
-			
+
 			JsonArray ja = aBuilder.build();
-			
+
 			writer.write(ja);
-		}catch (IOException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
-	
+
 	public static Person fromJson(String fileName) {
 		Person p = null;
-		try(JsonReader reader = Json.createReader(new FileReader(fileName))){
+		try (JsonReader reader = Json.createReader(new FileReader(fileName))) {
 			JsonObject jo = reader.readObject();
-			if(jo != null) {
+			if (jo != null) {
 				p = new Person();
 				p.setFirstName(jo.getString("firstName"));
+				p.setLastName(jo.getString("lastName"));
 				p.setAge(jo.getInt("age"));
-				
+
 				JsonObject joAdd = jo.getJsonObject("address");
-				Address address = new Address(
-						joAdd.getString("streetAddress"),
-						joAdd.getString("city"), 
-						joAdd.getString("state"), 
+				Address address = new Address(joAdd.getString("streetAddress"),
+						joAdd.getString("city"),
+						joAdd.getString("state"),
 						joAdd.getInt("postalCode"));
-				
+
 				p.setAddress(address);
-				
-				List<PhoneNumber> phones = new ArrayList<>(); 
+
+				List<PhoneNumber> phones = new ArrayList<>();
 				JsonArray ja = jo.getJsonArray("phoneNumbers");
-				for(JsonValue jv : ja) {
-					if(jv instanceof JsonObject) {
-						JsonObject joPh = (JsonObject)jv;
+				for (JsonValue jv : ja) {
+					if (jv instanceof JsonObject) {
+						JsonObject joPh = (JsonObject) jv;
 						phones.add(new PhoneNumber(joPh.getString("type"),
-								joPh.getString("number")));
+						joPh.getString("number")));
 					}
 				}
-				
+
 				p.setPhoneNumbers(phones);
 			}
-			
-		}catch (IOException e) {
+
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		return p;
 	}
-	
+
 	public static void main(String[] args) {
 		Address address = new Address("123 Main Street", "Cityville", "CA", 12345);
-        Person person = new Person("John", "Doe", 25, address);
+		Person person = new Person("John", "Doe", 25, address);
 
-        String jsonSinglePerson = JsonHandler.toJson(person);
-        System.out.println("JSON for single person:\n" + jsonSinglePerson);
+		String jsonSinglePerson = JsonHandler.toJson(person);
+		System.out.println("JSON for single person:\n" + jsonSinglePerson);
 
-        List<Person> personList = new ArrayList<>();
-        personList.add(person);
-        personList.add(new Person("Alice", "Smith", 30, null)); // Example with no address
+		List<Person> personList = new ArrayList<>();
+		personList.add(person);
+		personList.add(new Person("Alice", "Smith", 30, null)); // Example with no address
 
-        String jsonPersonList = JsonHandler.toJson(personList);
-        System.out.println("\nJSON for list of persons:\n" + jsonPersonList);
+		String jsonPersonList = JsonHandler.toJson(personList);
+		System.out.println("\nJSON for list of persons:\n" + jsonPersonList);
 
-        JsonHandler.toJsonFile(personList, "persons.json");
+		JsonHandler.toJsonFile(personList, "persons.json");
 
-//        List<Person> deserializedPersons = JsonHandler.getPersons("persons.json");
-        List<Person> deserializedPersons = JsonHandler.getPersons("persons.json");
-        System.out.println("\nDeserialized Persons:");
-        deserializedPersons.forEach(System.out::println);
+		List<Person> deserializedPersons = JsonHandler.getPersons("persons.json");
+		System.out.println("\nDeserialized Persons:");
+		deserializedPersons.forEach(System.out::println);
 	}
-	
+
 }
 //upcasting
 //downcasting
 
 //
-
